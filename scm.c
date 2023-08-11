856cce9b166555e7cb25997869b48f4ce25d8c20
lucascin <lesp@cin.ufpe.br>
2023-08-11 11:14:20 -0300

    Alteração para mostrar data e hora no printf.

--- scm.c
+++ scm.c
@@ -13,7 +13,20 @@ void subtrai(int a, int b) {
 }
 
 int main() {
-    printf("OlÃ¡, Estudante!\n");
+    // Alteração para mostrar data e hora no printf do programa
+	int hours, minutes, seconds, day, month, year;
+	time_t agora;	
+	time(&agora);
+	struct tm *local = localtime(&agora);
+	
+	hours = local->tm_hour;
+    minutes = local->tm_min;
+    seconds = local->tm_sec; 
+    day = local->tm_mday;
+    month = local->tm_mon + 1;
+    year = local->tm_year + 1900;
+    
+    printf("Olá, Estudante!\nSao %02d:%02d:%02d do dia %02d/%02d/%d.\n", hours, minutes, seconds, day, month, year);
     soma(5, 3);
     multiplica(5, 3);
     subtrai(5, 3);
