dd416894292b56f53c44faab75917fe00088e54d
vitorfccarvalho <vitorfassanaro@gmail.com>
2023-08-11 10:47:07 -0300

    Resolução do #2

--- scm.c
+++ scm.c
@@ -4,13 +4,11 @@ void soma(int a, int b) {
     printf("%d + %d = %d\n", a, b, a + b);
 }
 
-void multiplica(int a, int b) {
-    printf("%d x %d = %d\n", a, b, a * b);
-}
+//Aluno: Vitor Fassanaro
+//Remoção da função de multiplicação e sua chamada da main
 
 int main() {
     printf("Olá, Estudante!\n");
     soma(5, 3);
-    multiplica(5, 3);
     return 0;
 }
