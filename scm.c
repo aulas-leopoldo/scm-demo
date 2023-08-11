7f5fd41f474354321a7e37410225b955e1cb6f37
Caio Seixas Duarte <caio.seixasduarte@hotmail.com>
2023-08-11 10:53:29 -0300

    Implementação do nome do usuário

--- scm.c
+++ scm.c
@@ -9,7 +9,10 @@ void multiplica(int a, int b) {
 }
 
 int main() {
-    printf("Olá, Estudante!\n");
+    char nome[20];
+    printf("Digite seu nome:\n"); //Programa pede o nome do usuário
+    scanf("%s", nome);
+	printf("Olá, %s!\n", nome);
     soma(5, 3);
     multiplica(5, 3);
     return 0;
